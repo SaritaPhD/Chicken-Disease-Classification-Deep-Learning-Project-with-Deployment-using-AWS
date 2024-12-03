660f1aef165815253c98881fe18e060daba889b0
Sarita <126271555+SaritaPhD@users.noreply.github.com>
2024-12-03 11:43:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask_cors import CORS, cross_origin
 from cnnClassifier.utils.common import decodeImage
 from cnnClassifier.pipeline.predict import PredictionPipeline
 
-
 os.putenv('LANG', 'en_US.UTF-8')
 os.putenv('LC_ALL', 'en_US.UTF-8')
 
