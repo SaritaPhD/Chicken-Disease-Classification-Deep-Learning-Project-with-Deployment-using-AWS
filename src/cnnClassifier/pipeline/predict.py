eb8991d680e0123a989feed94552ffabd90ed0f7
Sarita <126271555+SaritaPhD@users.noreply.github.com>
2024-12-21 14:04:46 +0530

    Update predict.py

--- src/cnnClassifier/pipeline/predict.py
+++ src/cnnClassifier/pipeline/predict.py
@@ -3,7 +3,6 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import os
 
-
 class PredictionPipeline:
     def __init__(self,filename):
         self.filename =filename
